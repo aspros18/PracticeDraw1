40f03ce169a54489bc128c326f0ed33c6b849ecf
aspros18 <aimar18@126.com>
2018-04-10 23:11:39 +0800

    用Path画心形完成

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -2,11 +2,19 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice9DrawPathView extends View {
+
+    Path mPath = new Path();
+    Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     public Practice9DrawPathView(Context context) {
         super(context);
@@ -20,10 +28,21 @@ public class Practice9DrawPathView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawPath() 方法画心形
+
+        mPaint.setColor(Color.RED);
+
+        mPath.arcTo(400, 300, 550, 450, 150, 220, true);
+        mPath.arcTo(550, 300, 700, 450, 180, 220, false);
+        mPath.lineTo(550, 600);
+
+        mPath.close();
+
+        canvas.drawPath(mPath, mPaint);
     }
 }
